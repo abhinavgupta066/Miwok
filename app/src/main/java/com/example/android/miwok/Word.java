4aaa534a140ccb54b813199010f98243d93c18b5
abhinavgupta066 <abhinavgupta066@gmail.com>
2021-02-17 13:22:08 +0530

    final commit

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,44 @@
+package com.example.android.miwok;
+
+public class Word {
+
+    private String mDefaultTranslation;
+    private String mMiwokTranslation;
+    private int mImageResourceId=NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED=-1;
+
+    private int mAudioResourceId;
+
+    public Word(String DefaultTranslation, String MiwokTranslation, int AudioResourceId){
+        mDefaultTranslation = DefaultTranslation;
+        mMiwokTranslation = MiwokTranslation;
+        mAudioResourceId = AudioResourceId;
+    }
+
+    public Word(String DefaultTranslation, String MiwokTranslation, int ImageResourceId, int AudioResourceId){
+        mDefaultTranslation = DefaultTranslation;
+        mMiwokTranslation = MiwokTranslation;
+        mImageResourceId = ImageResourceId;
+        mAudioResourceId = AudioResourceId;
+    }
+
+    public String getmDefaultTranslation(){
+        return mDefaultTranslation;
+    }
+
+    public String getmMiwokTranslation(){
+        return mMiwokTranslation;
+    }
+
+    public int getmImageResourceId(){
+        return mImageResourceId;
+    }
+
+    public int getmAudioResourceId(){
+        return mAudioResourceId;
+    }
+
+    public boolean hasImage(){
+        return mImageResourceId!=NO_IMAGE_PROVIDED;
+    }
+}
